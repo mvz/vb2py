06b7415f31cf1fdc643addef2fb5e6ff9102d565
Matijs van Zuijlen <matijs@matijs.net>
2015-06-08 07:49:47 +0200

    Allow specifying rst source on command line

--- sandbox/makedocs.py
+++ sandbox/makedocs.py
@@ -13,7 +13,7 @@ import vb2py.extensions
 vb2py.extensions.disableLogging()
 vb2py.vbparser.log.setLevel(0) # Don't print all logging stuff
 
-# << fn doAutomaticVBConversion >>
+
 def doAutomaticVBConversion(txt):
     """Convert VB code in the text to Python"""
     vb = re.compile(r'(.*?)<p>VB(\(.*?\))?:</p>.*?<pre class="literal-block">(.*?)</pre>(.*?)',
@@ -35,27 +35,43 @@ def doAutomaticVBConversion(txt):
                                            m.renderAsCode(1).replace("\n", "<br>"),
                                            match.groups()[3])
     return vb.sub(convertVB, txt)
-# -- end -- << fn doAutomaticVBConversion >>
-# << fn addToTemplate >>
+
+
 def addToTemplate(text, template, token):
     """Add the text to the template"""
     template_text = open(template, "r").read()
     template_text = template_text.replace(token, text)
     return template_text
-# -- end -- << fn addToTemplate >>
+
 
 if __name__ == "__main__":
-    #
+
     add_to_template = 0
+    if len(sys.argv) == 1:
+        files = glob.glob(os.path.join("rst", "*.rst"))
+    else:
+        files = sys.argv[1:]
     settings = {
             'embed_stylesheet' : False,
             'stylesheet' : 'default.css',
             'stylesheet_path' : '',
             }
-    #
+
     print "\nvb2Py documentation generator\n"
-    for fn in glob.glob(os.path.join("rst", "*.rst")):
-        print "Processing '%s' ... " % fn,
+    for fn in files:
+        print "Processing '%s' ..." % fn,
+
+        if not fn.startswith("rst/"):
+            print "Source must be in rst/"
+            break
+        if not fn.endswith(".rst"):
+            print "Source must be reStructuredText"
+            break
+
+        target_file = "doc/" + fn[4:-4] + ".html"
+
+        print "to %s ..." % target_file,
+
         txt = open(fn, "r").read()
 
         base_html_text = publish_string(writer_name = 'html',
@@ -67,9 +83,6 @@ if __name__ == "__main__":
         if add_to_template:
             marked_up_text = addToTemplate(marked_up_text, sys.argv[2], "DOCSGOHERE")
 
-        base_name = os.path.basename(fn)
-
-        target_file = os.path.join("doc", base_name.replace(".rst", ".html"))
-
         open(target_file, "w").write(marked_up_text)
+
         print "Done!"
